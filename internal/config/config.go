4afab52b8fb4b17a1d386e7048981670ca12d29e
binaryCoder-101 <syed.misak@gmail.com>
2025-03-23 12:41:02 +0530

    config functions added

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,28 @@
 package config
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Config struct {
 	DbURL           string `json:"db_url"`
 	CurrentUserName string `json:"current_user_name"`
+}
+
+func (c *Config) SetUser(userName string, filePath string) error {
+	c.CurrentUserName = userName
+
+	jsonData, err := json.Marshal(c)
+	if err != nil {
+		return fmt.Errorf("error parsing config data struct")
+	}
+
+	err = os.WriteFile(filePath, jsonData, 0644)
+	if err != nil {
+		return fmt.Errorf("error writing to file")
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"github.com/binaryCoder-101/blog-aggregator/internal/config"
+)
+
+func main() {
+	cfg, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	filePath := homeDir + "/.gatorconfig.json"
+
+	cfg.SetUser("Misaque Ahmed", filePath)
+
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Fatal("error reading json data")
+	}
+
+	fmt.Printf(string(data))
 }
